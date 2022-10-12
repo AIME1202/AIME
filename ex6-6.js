bb8ad1c4114c3235bcf464b57eb8074a3c7e418a
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2022-10-12 16:12:59 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -7,7 +7,7 @@ function taro66() {
 }
 function taro66() {
   who66 = '太郎';
-  alert(who66+が来た +'!');
+  alert(who66+'が来た' +'!');
 }
 
 function hanako66() {
@@ -15,6 +15,6 @@ function hanako66() {
 }
 function hanako66() {
   who66 = '花子';
-  alert(who66+が来た '!');
+  alert(who66+'が来た' +'!');
 }
 
