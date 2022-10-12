fef40ee113ffa29f200982285682089938f914ec
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2022-10-12 15:43:06 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -3,18 +3,18 @@ function sayhello66() {
   alert('Hello, ' + who66 + '!');
 }
 function taro66() {
-  who65 = '太郎';
+  who66 = '太郎';
 }
 function taro66() {
-  who65 = '太郎';
+  who66 = '太郎';
   alert(who66+'が来た');
 }
 
 function hanako66() {
-  who65 = '花子';
+  who66 = '花子';
 }
 function hanako66() {
-  who65 = '花子';
+  who66 = '花子';
   alert(who66+'が来た');
 }
 
