6148617f12f4549ff9da75d7e86378d31d6f18dc
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2022-11-08 09:58:55 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,4 +1,4 @@
 function imgchange1()
 {
-document.getElementById('img1').innerHTML = '<img src="./image/城西大学２.jpg" alt='城西大学２'>';
+document.getElementById('img1').innerHTML = '<img src="./image/城西大学２.jpg">';
 }
