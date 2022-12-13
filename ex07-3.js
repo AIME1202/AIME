b9cc877ebba1f9f2bc650af55637800fe851cfa3
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2022-12-13 10:06:43 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,5 @@
 function add() {
-  Number x = document.getElementById('input1').value;
-  Number y = document.getElementById('input2').value;
-  alert(Number x+Number y);
+  var x = document.getElementById('input1').value;
+  var y = document.getElementById('input2').value;
+  alert(x+y);
 }
