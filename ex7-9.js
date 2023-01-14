3e3ad544c5392119fc19cf89d193bb23cf9ee73a
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2023-01-14 11:59:40 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,17 @@
+function nazo4(){
+if(document.getElementById('answer4').value=='夢'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer4').value=='ゆめ'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer4').value=='ユメ'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer4').value=='yume'){
+ alert('あたり！');
+}
+else{
+ alert('はずれ!');
+ }
+}
